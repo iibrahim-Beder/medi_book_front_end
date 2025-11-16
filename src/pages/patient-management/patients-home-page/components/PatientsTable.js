878530d27d277434870c812c34a8f09b0036904c
m7mmmmoud-elshikh <m7mmmmoud1@gmail.com>
2025-11-16 23:09:54 +0200

    refactor: improve styling in patients-home-page =>  PatientManagement page

--- src/pages/patient-management/patients-home-page/components/PatientsTable.js
+++ src/pages/patient-management/patients-home-page/components/PatientsTable.js
@@ -7,6 +7,7 @@ import { MdOutlineArrowForward } from "react-icons/md";
 import MainSearch from "../../../shared/MainSearch";
 import DateRangePicker from "../../patient-information/PatientTabs/component/DateRangePicker";
 import FilterDropdown from "../../patient-information/PatientTabs/component/FilterDropdown";
+import { CiSearch } from "react-icons/ci";
 
 const PatientsTable = () => {
   const { t } = useTranslation();
@@ -139,52 +140,34 @@ const PatientsTable = () => {
          <div 
          className="filters-container-search" 
          >
-         <MainSearch />
+         {/* <MainSearch /> */}
+         <div style={{ position: "relative" }}>
+            <input
+              className="form-control small-search normal-search"
+              type="text"
+              placeholder={t("search for patient")}
+              style={{height:"100px"}}
+              // value={searchTerm}
+              // onChange={(e) => setSearchTerm(e.target.value)}
+              // onKeyDown={(e) => {
+              //   if (e.key === "Enter") handleSearch();
+              // }}
+            />
+            <CiSearch
+              style={{
+                position: "absolute",
+                left: "10px",
+                top: "50%",
+                transform: "translateY(-50%)",
+                color: "#000",
+                fontSize: "23px",
+              }}
+            />
+          </div>
              <div className=" review-filters ">
              
              <FilterDropdown small/>
              <DateRangePicker/>
-
-            {/* <ConditionsFilters
-              searchTerm={searchTerm}
-              setSearchTerm={setSearchTerm}
-              filterServiceType={filterServiceType}
-              setFilterServiceType={setFilterServiceType}
-              filterRating={filterRating}
-              setFilterRating={setFilterRating}
-              filterDateFrom={filterDateFrom}
-              setFilterDateFrom={setFilterDateFrom}
-              filterDateTo={filterDateTo}
-              setFilterDateTo={setFilterDateTo}
-              onReset={resetFilters}
-              onSearch={handleSearch}
-              conditions={visitTypes}
-              showSearchInput={true}
-              showDateRange={true}
-              showFilterDropdown={true}
-              customFilters={[
-                {
-                  name: "rating",
-                  label: "Rating",
-                  data: [
-                    { key: "1", label: "1 Star" },
-                    { key: "2", label: "2 Stars" },
-                    { key: "3", label: "3 Stars" },
-                    { key: "4", label: "4 Stars" },
-                    { key: "5", label: "5 Stars" },
-                  ],
-                },
-                {
-                  name: "serviceType",
-                  label: "Visit Type",
-                  data: [
-                    { key: "Video Call", label: "Video Call" },
-                    { key: "Voice Call", label: "Voice Call" },
-                    { key: "In-Person Visit", label: "In-Person Visit" },
-                  ],
-                },
-              ]}
-            /> */}
           </div>
        </div>
 
--- src/pages/patient-management/patients-home-page/components/StatCard.js
+++ src/pages/patient-management/patients-home-page/components/StatCard.js
@@ -3,19 +3,12 @@ import { ArrowUp, ArrowDown } from "lucide-react";
 const StatCard = ({ title, value, percentage, isPositive, icon: Icon, color }) => {
   return (
     <div className="col-xl-3 col-sm-6 col-12 stats-card">
-      <div className="table-card" style={{ border: "1px solid #E6E8EE" }}>
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            flexWrap: "nowrap",
-            justifyContent: "flex-start",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: "10px",
-          }}
-        >
-          <div
+      <div className="dc-insightsitem dc-dashboardbox stats-card-item">
+        <div>
+          <figure className="dc-userlistingimg">
+          <img alt="icon " src="/images/thumbnail/img-19.png" ></img>
+          </figure>
+          {/* <div
             className="d-flex align-items-center justify-content-center  mb-3 "
             style={{
               borderRadius: "10px",
@@ -26,40 +19,44 @@ const StatCard = ({ title, value, percentage, isPositive, icon: Icon, color }) =
             }}
           >
             <Icon size={30} />
-          </div>
+          </div> */}
           <h4 className=" stats-card-title " style={{ textAlign: "center" }}>
             {title}
           </h4>
         </div>
         <div
-          style={{
-            display: "flex",
-            width: "100%",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: "10px",
-          }}
+          // style={{
+          //   display: "flex",
+          //   width: "100%",
+          //   flexWrap: "wrap",
+          //   justifyContent: "center",
+          //   flexDirection: "column",
+          //   alignItems: "center",
+          //   gap: "10px",
+          // }}
         >
           {/* Value */}
-          <h3 className="fw-bold mb-2">{value}</h3>
-           <div>
-          <span
-            className={`badge rounded-pill px-2 py-1 ${
-              isPositive
-                ? "bg-success-subtle text-success"
-                : "bg-danger-subtle text-danger"
-            }`}
+          <h3 className="fw-bold mb-0 ">{value}</h3>
+          <div className=" prs-state ">
+            <span
+              className={`badge rounded-pill px-2 py-1 ${
+                isPositive
+                  ? "bg-success-subtle text-success"
+                  : "bg-danger-subtle text-danger"
+              }`}
             >
-            {isPositive ? <ArrowUp size={14} /> : <ArrowDown size={14} />}
-            <span className="ms-1">{percentage}%</span>
-          </span>
-          <small className={` ms-2  ${
-              isPositive
-                ? "bg-success-subtle text-success"
-                : "bg-danger-subtle text-danger"
-            }`}>since last month</small>
+              {isPositive ? <ArrowUp size={14} /> : <ArrowDown size={14} />}
+              <span className="ms-1">{percentage}%</span>
+            </span>
+            <small
+              className={` ms-2  ${
+                isPositive
+                  ? "bg-success-subtle text-success"
+                  : "bg-danger-subtle text-danger"
+              }`}
+            >
+              since last month
+            </small>
           </div>
         </div>
       </div>
